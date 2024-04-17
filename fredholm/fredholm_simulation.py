991393f41d5ae9eba3fa9c84e5806c41425f77c4
harry <harrygietz@gmail.com>
2024-04-16 22:24:40 -0500

    PEP8 compliance for fredholm_simulation.py

--- fredholm/fredholm_simulation.py
+++ fredholm/fredholm_simulation.py
@@ -8,6 +8,15 @@ from .fredholm_datagen_utils import create_data, b1_v, b2_v, b_bar
 from .fredholm_utils import FredholmGlobLoc
 
 def parse_args():
+    """
+    Parse the command-line arguments for the Fredholm simulation.
+
+    This function sets up and parses command-line arguments necessary for configuring the simulation.
+    The results, including various parameters of the distribution and simulation settings, will be saved to a specified file path.
+
+    Returns:
+        argparse.Namespace: An object that contains the arguments and their values as attributes.
+    """
     parser = argparse.ArgumentParser(description="Run the Fredholm simulation with configurable parameters.")
     parser.add_argument('--results_filepath', type=str, required=True,
                         help='filepath to save the results (should be a .pkl).')
@@ -49,12 +58,72 @@ function_map = {
 
 
 class FredSimulation:
+    """
+    Class to create training data and run the gibbs learning process using the Fredholm approach.
+
+    Attributes:
+        results_filepath (str): Path where the simulation results will be saved.
+        scale (float): Scale parameter for the truncated normal distribution used for generating data.
+        loc (float): Location parameter for the truncated normal distribution used for generating data.
+        a_trunc (float): Lower bound for the truncation for the normal used for generating data.
+        b_trunc (float): Upper bound for the truncation for the normal used for generating data.
+        diffusion (float): Diffusion coefficient of the SDE.
+        t_delta (float): Time delta for simulation steps.
+        t_end (float): Ending time for the simulation.
+        start_val (float): Initial value for the simulation.
+        gibbs_iters (int): Number of Gibbs iterations to perform.
+        chunk_size (int): Size of chunks for B integral calculation, prior to running the Gibbs sampling.
+        integral_n (int): Number of points for numerical integration.
+        y_domain (tuple): Domain of the Y variable as a tuple (low, high).
+        b (callable): Function to be used in the simulation of data and the learning algorithm (b1_v or b2_v).
+        local_gig_a (float): 'a' parameter for local GIG distribution on the learned parameters.
+        local_gig_b (float): 'b' parameter for local GIG distribution on the learned parameters.
+        local_gig_p (float): 'p' parameter for local GIG distribution on the learned parameters.
+        global_gig_a (float): 'a' parameter for global GIG distribution on the learned parameters.
+        global_gig_b (float): 'b' parameter for global GIG distribution on the learned parameters.
+        global_gig_p (float): 'p' parameter for global GIG distribution on the learned parameters.
+        kernel_name (str): Name of the kernel function used.
+
+    Methods:
+        run_simulation: Executes the simulation and saves the results.
+    """
     def __init__(self, results_filepath, scale=np.sqrt(10), loc=1, a_trunc=-100, b_trunc=100,
                  diffusion=1, t_delta=0.05, t_end=100, start_val=0,
                  gibbs_iters=40, chunk_size=150, integral_n=5000,
                  y_domain=(-100, 100), b=b1_v, a_loc=2, b_loc=0,
                  p_loc=1, a_glob=2, b_glob=0, p_glob=1,
                  kernel_name='gauss'):
+        """
+        Initialize a simulation instance with specified parameters for running Fredholm SDE learning simulations.
+
+        Parameters:
+            results_filepath (str): The file path where the simulation results will be stored.
+            scale (float): The scale parameter for the truncated normal distribution used for data generation.
+            loc (float): The location parameter for the truncated normal distribution used for data generation.
+            a_trunc (float): The lower bound for truncation in the truncated normal distribution used for data
+            generation.
+            b_trunc (float): The upper bound for truncation in the distribution used for data generation.
+            diffusion (float): The diffusion coefficient of the SDE data.
+            t_delta (float): The time delta or step size for the SDE.
+            t_end (float): The end time of the generated data.
+            start_val (float): The starting value from which the generated data will begin.
+            gibbs_iters (int): The number of Gibbs sampling iterations to perform during the learning process.
+            chunk_size (int): The size of chunks used for calculating the B integral in parts, prior to the gibbs
+            process.
+            integral_n (int): The number of integral points used in numerical integration methods.
+            y_domain (tuple of float): The range of the Y variable in the simulation, specified as (low, high).
+            b (callable): The function defining the behavior over time, selected from predefined functions.
+            a_loc (float): The 'a' parameter for the local Generalized Inverse Gaussian (GIG) distribution.
+            b_loc (float): The 'b' parameter for the local GIG distribution.
+            p_loc (float): The 'p' parameter for the local GIG distribution.
+            a_glob (float): The 'a' parameter for the global GIG distribution.
+            b_glob (float): The 'b' parameter for the global GIG distribution.
+            p_glob (float): The 'p' parameter for the global GIG distribution.
+            kernel_name (str): The name of the kernel function used in the simulation's calculations.
+
+        Sets up the Fredholm learning simulation environment based on the provided parameters, preparing for a run that
+        can be executed using the method `run_simulation`.
+        """
         self.scale = scale
         self.loc = loc
         self.a_trunc = a_trunc
@@ -78,6 +147,12 @@ class FredSimulation:
         self.kernel_name = kernel_name
 
     def run_simulation(self):
+        """
+        Run the Fredholm simulation using the initialized settings and parameters.
+
+        This method generates the necessary data, performs the gibbs learning process through the
+        FredholmGlobLoc object, and finally, saves the results to the specified file path.
+        """
         np.random.seed(0)
         a_trunc_real, b_trunc_real = (self.a_trunc - self.loc) / self.scale, (self.b_trunc - self.loc) / self.scale
         pi = truncnorm(a_trunc_real, b_trunc_real, loc=self.loc, scale=self.scale)
@@ -101,35 +176,17 @@ class FredSimulation:
                                             )
         fred_glob_loc_obj.run_gibbs(matrix_calc='chunked')
 
-        fred_results_dict = {
-            'meta_data': {
-                'scale': self.scale,
-                'loc': self.loc,
-                'a_trunc': self.a_trunc,
-                'b_trunc': self.b_trunc,
-                'diffusion': self.diffusion,
-                't_delta': self.t_delta,
-                't_end': self.t_end,
-                'start_val': self.start_val,
-                'gibbs_iters': self.gibbs_iters,
-                'chunk_size': self.chunk_size,
-                'integral_n': self.integral_n,
-                'y_domain': self.y_domain,
-                'results_filepath': self.results_filepath,
-                'local_gig_a': self._local_gig_a,
-                'local_gig_b': self._local_gig_b,
-                'local_gig_p': self._local_gig_p,
-                'global_gig_a': self._global_gig_a,
-                'global_gig_b': self._global_gig_b,
-                'global_gig_p': self._global_gig_p,
-                'kernel_name': self.kernel_name
-            },
-            'beta_record': fred_glob_loc_obj.beta_record,
-            'y_domain': fred_glob_loc_obj.y_domain,
-            'init_data': fred_glob_loc_obj.init_data,
-            'known_b': fred_glob_loc_obj.known_b,
-            'diffusion': fred_glob_loc_obj.diffusion
-        }
+        fred_results_dict = dict(
+            meta_data=dict(scale=self.scale, loc=self.loc, a_trunc=self.a_trunc, b_trunc=self.b_trunc,
+                           diffusion=self.diffusion, t_delta=self.t_delta, t_end=self.t_end,
+                           start_val=self.start_val, gibbs_iters=self.gibbs_iters, chunk_size=self.chunk_size,
+                           integral_n=self.integral_n, y_domain=self.y_domain, results_filepath=self.results_filepath,
+                           local_gig_a=self._local_gig_a, local_gig_b=self._local_gig_b, local_gig_p=self._local_gig_p,
+                           global_gig_a=self._global_gig_a, global_gig_b=self._global_gig_b,
+                           global_gig_p=self._global_gig_p, kernel_name=self.kernel_name),
+            beta_record=fred_glob_loc_obj.beta_record, y_domain=fred_glob_loc_obj.y_domain,
+            init_data=fred_glob_loc_obj.init_data, known_b=fred_glob_loc_obj.known_b,
+            diffusion=fred_glob_loc_obj.diffusion)
 
         os.makedirs(os.path.dirname(self.results_filepath), exist_ok=True)
 
